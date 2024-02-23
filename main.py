aa35b5ad7cfd2f9b9d0e53fd3ae1a6d40aba5963
vincentlc <vincent.lc@laposte.net>
2024-02-23 11:25:08 -0300

    publication main Vincent

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import json
+# Code Vincent
+
+base_url = "https://impactco2.fr/api/v1/"
+thematics = "thematiques"
+ecv = "ecv"
+# Get the thematics
+response = requests.get(base_url + thematics)
+assert response.status_code == 200
+json_response = response.json()
+
+# Printing the categories and values
+# for category in response.json().get("data", []):
+#     print(" ".join([category.get("emoji",""),category.get("name")]))
+
+# Get the slug of all thematics
+category_list = []
+for category in response.json().get("data", []):
+    category_list.append(category.get("slug"))
+print(category_list)
+category_list = category_list[:-1]  # remove the "divers", which is empty
+
+# Get ecv of the first category
+for category in category_list:
+    response_category = requests.get("/".join([base_url, thematics, ecv, category]))
+    assert response_category.status_code == 200
+    print(json.dumps(response_category.json(), indent=4))  # print the result
